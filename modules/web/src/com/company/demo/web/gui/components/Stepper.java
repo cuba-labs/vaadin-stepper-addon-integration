046aa616e8a578b17bc50da4d6ebe75c5bd15309
Gleb Gorelov <gorelov@haulmont.com>
2019-02-04 12:51:01 +0400

    Addon implementation

--- modules/web/src/com/company/demo/web/gui/components/Stepper.java
+++ modules/web/src/com/company/demo/web/gui/components/Stepper.java
@@ -1,7 +1,24 @@
 package com.company.demo.web.gui.components;
 
-import com.haulmont.cuba.gui.components.Component;
+import com.haulmont.cuba.gui.components.Field;
 
-public interface Stepper extends Component {
+// note that Stepper should extend Field
+public interface Stepper extends Field {
+
     String NAME = "stepper";
+
+    boolean isManualInputAllowed();
+    void setManualInputAllowed(boolean value);
+
+    boolean isMouseWheelEnabled();
+    void setMouseWheelEnabled(boolean value);
+
+    int getStepAmount();
+    void setStepAmount(int amount);
+
+    int getMaxValue();
+    void setMaxValue(int maxValue);
+
+    int getMinValue();
+    void setMinValue(int minValue);
 }
--- modules/web/src/com/company/demo/web/gui/xml/layout/loaders/StepperLoader.java
+++ modules/web/src/com/company/demo/web/gui/xml/layout/loaders/StepperLoader.java
@@ -1,9 +1,9 @@
 package com.company.demo.web.gui.xml.layout.loaders;
 
 import com.company.demo.web.gui.components.Stepper;
-import com.haulmont.cuba.gui.xml.layout.loaders.AbstractComponentLoader;
+import com.haulmont.cuba.gui.xml.layout.loaders.AbstractFieldLoader;
 
-public class StepperLoader extends AbstractComponentLoader<Stepper> {
+public class StepperLoader extends AbstractFieldLoader<Stepper> {
     @Override
     public void createComponent() {
         resultComponent = factory.createComponent(Stepper.class);
@@ -12,5 +12,28 @@ public class StepperLoader extends AbstractComponentLoader<Stepper> {
 
     @Override
     public void loadComponent() {
+
+        super.loadComponent();
+
+        String manualInput = element.attributeValue("manualInput");
+        if (manualInput != null) {
+            resultComponent.setManualInputAllowed(Boolean.parseBoolean(manualInput));
+        }
+        String mouseWheel = element.attributeValue("mouseWheel");
+        if (mouseWheel != null) {
+            resultComponent.setMouseWheelEnabled(Boolean.parseBoolean(mouseWheel));
+        }
+        String stepAmount = element.attributeValue("stepAmount");
+        if (stepAmount != null) {
+            resultComponent.setStepAmount(Integer.parseInt(stepAmount));
+        }
+        String maxValue = element.attributeValue("maxValue");
+        if (maxValue != null) {
+            resultComponent.setMaxValue(Integer.parseInt(maxValue));
+        }
+        String minValue = element.attributeValue("minValue");
+        if (minValue != null) {
+            resultComponent.setMinValue(Integer.parseInt(minValue));
+        }
     }
 }
