cda44ea01274d49b36aa7b4ab513d5f61a3e76f4
Gleb Gorelov <gorelov@haulmont.com>
2019-02-04 13:00:50 +0400

    Add default value for a new entity

--- modules/web/src/com/company/demo/web/customer/CustomerEdit.java
+++ modules/web/src/com/company/demo/web/customer/CustomerEdit.java
@@ -4,4 +4,8 @@ import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.company.demo.entity.Customer;
 
 public class CustomerEdit extends AbstractEditor<Customer> {
+    @Override
+    protected void initNewItem(Customer item) {
+        item.setScore(0);
+    }
 }
